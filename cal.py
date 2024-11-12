03aa333af677f2690c54b57c5193bd9e19bb42ba
Hackerleo35 <hassankhattal11@gmail.com>
2024-11-12 12:04:30 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+addition=num1+num2
+subtraction=num1-num2
+multiply=num1*num2
+divide=num1/num2
+print("The sum of num1 and num2 is",addition)
+print("The difference of num1 and num2 is",subtraction)
+print("The product of num1 and num2 is",multiply)
+print("The remainder of num1 and num2 is",divide)
